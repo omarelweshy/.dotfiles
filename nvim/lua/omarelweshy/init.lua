53cc683aaae09a8849a56403896e8557b87a81cc
Omar Elweshy <omarelweshy@gmail.com>
2022-07-29 23:20:42 +0400

    more telescope

--- nvim/lua/omarelweshy/init.lua
+++ nvim/lua/omarelweshy/init.lua
@@ -5,3 +5,20 @@ require("omarelweshy.telescope");
 require("omarelweshy.cmp");
 require("omarelweshy.lspconfig");
 require("omarelweshy.keymaps");
+
+local augroup = vim.api.nvim_create_augroup
+ThePrimeagenGroup = augroup('omarelweshy', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 50,
+        })
+    end,
+})
